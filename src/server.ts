7b685df1c2c661487a0116975663245e32a451d7
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-07-10 16:24:48 -0300

    feat: add confirm-trip route

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,14 @@ import {
 } from "fastify-type-provider-zod";
 
 import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
 
 const app = fastify();
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
