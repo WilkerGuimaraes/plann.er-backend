b8a0f94228b662361c53118bc84eb8eb327f2e9d
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-07-24 18:31:34 -0300

    fix: rename property emails_to_invite

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -20,7 +20,7 @@ export async function createTrip(app: FastifyInstance) {
           ends_at: z.coerce.date(),
           owner_name: z.string(),
           owner_email: z.string().email(),
-          email_to_invite: z.array(z.string().email()),
+          emails_to_invite: z.array(z.string().email()),
         }),
       },
     },
@@ -31,7 +31,7 @@ export async function createTrip(app: FastifyInstance) {
         ends_at,
         owner_name,
         owner_email,
-        email_to_invite,
+        emails_to_invite,
       } = request.body;
 
       if (dayjs(starts_at).isBefore(new Date())) {
@@ -56,7 +56,7 @@ export async function createTrip(app: FastifyInstance) {
                   is_owner: true,
                   is_confirmed: true,
                 },
-                ...email_to_invite.map((email) => {
+                ...emails_to_invite.map((email) => {
                   return { email };
                 }),
               ],
